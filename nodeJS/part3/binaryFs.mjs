7f5938efa7776ffc45a9eed5a145bb15e64e11e6
Grant Hayes <228530441+granthayes2006-code@users.noreply.github.com>
2025-11-25 09:36:02 -0600

    Commit node.js lab

---
+++ nodeJS/part3/binaryFs.mjs
@@ -0,0 +1,20 @@
+import fs from 'node:fs';
+
+const buffer = Buffer.from('Hello, Buffer!', 'utf8');
+console.log(buffer); // Output the Buffer
+
+fs.writeFile('binaryData.data', buffer, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Binary data written to binaryData.data');
+
+  fs.readFile('binaryData.data', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Read binary data:', data.toString('utf8'));
+  });
+});
