276949e2136a5ba5d39d947ac338236ee5db7dfd
Grant Hayes <228530441+granthayes2006-code@users.noreply.github.com>
2025-11-26 00:32:48 -0600

    Commit React Lab 2

---
+++ reactJS/.vs/reactJS/thumbs-rating-app/src/components/Product.jsx
@@ -0,0 +1,10 @@
+const Product = () => {
+  return (
+    <div className="product">
+      <h2>Fictional Product</h2>
+      <p>This is an amazing product. You can give it a thumbs up or thumbs down!</p>
+    </div>
+  );
+};
+
+export default Product;
